2b8c30c9e15aed739e12f90723600069257c0e6d
MohammedShama29 <61698708+MohammedShama29@users.noreply.github.com>
2020-03-17 17:36:44 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class Class1
+    {
+        public static int AddTen(int value)
+        {
+            return value + 10;
+        }
+    }
+}
